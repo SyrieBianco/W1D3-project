55bbb167fbd66faad3a5b6cf3a7b8561a9c0d20d
Syrie Bianco <syriebianco@gmail.com>
2017-06-16 10:59:36 -0700

    Make my_attr_accessor macro

--- Projects/ActiveRecordLite/lib/00_attr_accessor_object.rb
+++ Projects/ActiveRecordLite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,18 @@
 class AttrAccessorObject
+
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(':#{name}') do
+        self.instance_variable_get(:@name)
+      end
+    end
+
+    names.each do |name|
+      define_method(':#{name}=') do
+        self.instance_variable_set(:@name, name)
+      end
+    end
   end
+
+
 end
