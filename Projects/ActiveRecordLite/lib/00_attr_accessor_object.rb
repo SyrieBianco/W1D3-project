7065dc6412d341bb7650853cb34fb4fa3ff9318b
Syrie Bianco <syriebianco@gmail.com>
2017-06-16 12:09:18 -0700

    Add column getter and setter methods and attribute hash

--- Projects/ActiveRecordLite/lib/00_attr_accessor_object.rb
+++ Projects/ActiveRecordLite/lib/00_attr_accessor_object.rb
@@ -2,14 +2,12 @@ class AttrAccessorObject
 
   def self.my_attr_accessor(*names)
     names.each do |name|
-      define_method(':#{name}') do
-        self.instance_variable_get(:@name)
+      define_method(name) do
+        instance_variable_get("@#{name}")
       end
-    end
 
-    names.each do |name|
-      define_method(':#{name}=') do
-        self.instance_variable_set(:@name, name)
+      define_method("#{name}=") do |value|
+        instance_variable_set("@#{name}", value)
       end
     end
   end
--- Projects/ActiveRecordLite/lib/01_sql_object.rb
+++ Projects/ActiveRecordLite/lib/01_sql_object.rb
@@ -4,19 +4,41 @@ require 'active_support/inflector'
 # of this project. It was only a warm up.
 
 class SQLObject
+
+
   def self.columns
-    # ...
+    return @columns if @columns
+    columns = (DBConnection.execute2(<<-SQL)
+    SELECT *
+    FROM #{self.table_name}
+    SQL
+    ).first
+
+    columns.map!(&:to_sym)
+    @columns = columns
   end
 
   def self.finalize!
+    self.columns.each do |column|
+
+      define_method(column) do
+        attributes[column]
+      end
+
+      define_method("#{column}=") do |value|
+        attributes[column] = value
+      end
+
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
+
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.name.tableize
   end
 
   def self.all
@@ -36,7 +58,7 @@ class SQLObject
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
