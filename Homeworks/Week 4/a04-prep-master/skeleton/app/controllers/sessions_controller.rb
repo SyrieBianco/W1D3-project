7ee6e46207d7b625e9c6099d8a234622242e2385
Syrie Bianco <syriebianco@gmail.com>
2017-06-22 06:47:09 -0700

    First pass at controllers

--- Homeworks/Week 4/a04-prep-master/skeleton/app/controllers/sessions_controller.rb
+++ Homeworks/Week 4/a04-prep-master/skeleton/app/controllers/sessions_controller.rb
@@ -1,13 +1,26 @@
 class SessionsController < ApplicationController
 
   def new
+    render :new
   end
 
   def create
+    @user = User.find_by_credentials(
+    params[:user][:username],
+    params[:user][:password])
+
+    if @user.nil?
+      flash.now[:errors] = ["invalid username or password"]
+      render :new
+    else
+      login!(user)
+      redirect_to links_url
+    end
   end
 
   def destroy
-
+    current_user.try.reset_session_token!
+    session[:session_token] = nil
   end
 
 
