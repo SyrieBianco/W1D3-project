e9b2f20788aefbaac1a92c45fcfd7c23645e6d8c
Syrie Bianco <syriebianco@gmail.com>
2017-06-23 07:25:07 -0700

    Rearrange folders, download completed Music App

--- Projects/Music/app/controllers/users_controller.rb
+++ Projects/Music/app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(user)
+      redirect_to user_url
+    else
+      
+    end
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end
