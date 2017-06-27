f9397d4dcde40b424dfd57170f9871a427d0750a
Syrie Bianco <syriebianco@gmail.com>
2017-06-27 08:24:40 -0700

    Complete controllers phase, 8:24am

--- Homeworks/Week 4/Practice Tests/Practice Test 8/skeleton/config/routes.rb
+++ Homeworks/Week 4/Practice Tests/Practice Test 8/skeleton/config/routes.rb
@@ -1,2 +1,6 @@
 Links::Application.routes.draw do
+resources :users, only: [:new, :create, :destroy]
+resources :comments, only: [:new, :create, :destroy]
+resources :links
+resource :session, only: [:new, :create, :destroy]
 end
