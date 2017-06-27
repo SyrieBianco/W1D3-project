17b20ca9d5fdea57feba7c5f1e385eb34da87a46
Syrie Bianco <syriebianco@gmail.com>
2017-06-26 19:48:28 -0700

    Complete controllers phase, 7:48pm

---
+++ Homeworks/Week 4/Practice Tests/Practice Test 3/skeleton/app/controllers/links_controller.rb
@@ -0,0 +1,50 @@
+class LinksController < ApplicationController
+  before_action :require_logged_in
+
+  def create
+    @link = current_user.links.new(link_params)
+
+    if @link.save
+      redirect_to link_url(@link)
+    else
+      flash.now[:errors] = @link.errors.full_messages
+      render :new
+    end
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def index
+    @links = Link.all
+  end
+
+  def show
+    @link = Link.find(params[:id])
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = current_user.links.find(params[:id])
+
+    if @link.update_attributes(link_params)
+      redirect_to link_url(@link)
+    else
+      flash.now[:errors] = @link.errors.full_messages
+      render :edit
+    end
+  end
+
+  # def destroy
+  # end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:title, :url)
+  end
+end
--- Homeworks/Week 4/Practice Tests/Practice Test 3/skeleton/config/routes.rb
+++ Homeworks/Week 4/Practice Tests/Practice Test 3/skeleton/config/routes.rb
@@ -1,2 +1,7 @@
 Links::Application.routes.draw do
+  resources :users, only: [:create, :new]
+  resources :links
+  resources :comments, only: [:create, :destroy]
+  resource :session, only: [:create, :new, :destroy]
+
 end
