4404dd2ffd6b74f3f781b80e70a1aeeb294ae373
Syrie Bianco <syriebianco@gmail.com>
2017-06-28 16:26:07 -0700

    Reorganize folders

---
+++ Homeworks/Week 4/Practice Tests/syrie_bianco/app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  before_action :require_logged_in
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
