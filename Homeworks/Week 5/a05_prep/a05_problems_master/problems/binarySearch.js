4ccaf083faf018fca747263bf32d725c7c4d786f
Syrie Bianco <syriebianco@gmail.com>
2017-07-06 18:12:13 -0700

    Add week 6 folder, skeleton

--- Homeworks/Week 5/a05_prep/a05_problems_master/problems/binarySearch.js
+++
@@ -1,7 +0,0 @@
-function myBsearch(array, target) {
-
-}
-
-console.log(myBsearch([1,2,3,4,5], 2) ); // 1
-console.log(myBsearch([1,2,3,4,5], 5)); // 4
-console.log(myBsearch([1,2,3,4,5], 6)); // -1
