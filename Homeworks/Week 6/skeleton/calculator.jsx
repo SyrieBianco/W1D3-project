c86c9acc508f78597d2783876b62ae689f82ede0
Syrie Bianco <syriebianco@gmail.com>
2017-07-07 07:02:55 -0700

    Complete React Calculatior

--- Homeworks/Week 6/skeleton/calculator.jsx
+++ Homeworks/Week 6/skeleton/calculator.jsx
@@ -7,29 +7,70 @@ class Calculator extends React.Component {
       result: 0,
       num1: "",
       num2: ""
-    }
+    } ;
+
     this.setNum1 = this.setNum1.bind(this);
     this.setNum2 = this.setNum2.bind(this);
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.divide = this.divide.bind(this);
+    this.clear = this.clear.bind(this);
   }
 
   setNum1 (e) {
     const num1 = e.target.value ? parseInt(e.target.value) : "";
-    this.state.num1 = num1
+    this.setState({num1});
   }
 
   setNum2 (e) {
     const num2 = e.target.value ? parseInt(e.target.value) : "";
-    this.state.num2 = num2
+    this.setState({num2});
+  }
+
+  add (e) {
+    e.preventDefault();
+    const result = this.state.num1 + this.state.num2;
+    this.setState({result});
+  }
+
+  subtract (e) {
+    e.preventDefault();
+    const result = this.state.num1 - this.state.num2;
+    this.setState({result});
+  }
+
+  multiply (e) {
+    e.preventDefault();
+    const result = this.state.num1 * this.state.num2;
+    this.setState({result});
+  }
+
+  divide (e) {
+    e.preventDefault();
+    const result = this.state.num1 / this.state.num2;
+    this.setState({result});
+  }
+
+  clear (e) {
+    e.preventDefault();
+    this.setState({result : 0});
   }
 
 
-
   render() {
+    const {num1, num2, result} = this.state;
     return (
       <div>
         <h1>{this.state.result}</h1>
-        <input type="text" name="" onChange={this.setNum1} value={this.state.num1}>// replace this with your code
-        <input type="text" name="" onChange={this.setNum2} value={this.state.num2}>// replace this with your code
+        <input type="text" name="" onChange={this.setNum1} value={this.state.num1}/>
+        <input type="text" name="" onChange={this.setNum2} value={this.state.num2}/>
+        <br></br>
+        <button onClick={this.add}> + </button>
+        <button onClick={this.subtract}> - </button>
+        <button onClick={this.multiply}> * </button>
+        <button onClick={this.divide}> / </button>
+        <button onClick={this.clear}> Clear </button>
       </div>
     );
   }
